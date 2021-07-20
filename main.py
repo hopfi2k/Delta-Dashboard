1910edf9c91a898f4f3754abde879b2319796251
hopfi2k@me.com <+Nick4nack.08>
2021-07-20 15:19:41 +0200

    fixes

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 
 import json
 import socket
+import sys
 
 from pymodbus.version import version
 from pymodbus.server.asynchronous import StartTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.diag_message import *
+from pymodbus.file_message import *
+from pymodbus.other_message import *
+from pymodbus.mei_message import *
 import asyncio
 import os
 import logging
@@ -21,12 +26,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import websockets
 import random
 
+# check if correct Python version is installed
+if sys.version_info < (3, 7):
+    sys.exit('This program requires min. Python version 3.7. PLease update Python. Aborting...')
 
 # define constants
 # HTTP_HOST_IP = socket.gethostbyname_ex(socket.gethostname())[-1][0]               # retreive the host name
-HTTP_HOST_IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+HTTP_HOST_IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or
+                 [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())
+                   for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+
 HTTP_PORT = int(os.getenv('PORT', 8080))    # http port the dashboard will bind to
-RS485_PORT = '/dev/ttyUSB0'                 # USB device of the RS-485 adapter (inverter)
+RS485_DEVICE = '/dev/ttyUSB0'                 # USB device of the RS-485 adapter (inverter)
 RS485_READ_INTERVAL = 1                     # read values every second
 UNIT = 0x00                                 # unit ID of the Sunspec slave, default
 
@@ -37,7 +48,7 @@ FORMAT = ('%(asctime)-15s %(threadName)-15s'
           ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)             # set to .INFO for production
+log.setLevel(logging.INFO)             # set to .INFO for production
 
 
 #
@@ -47,11 +58,24 @@ class RS485ReaderClass(threading.Thread, sunspec.delta_data_structure.DeltaDataS
     new_data = False  # true if new data is available
     timestamp = None  # timestamp of last data update
 
+    # Class constructor
     def __init__(self, rs485_port):
         threading.Thread.__init__(self)  # call parent constructor
-        self.rs485port = rs485_port
+        self.client = ModbusClient(method='rtu', port=rs485_port, timeout=1, baudrate=9600)   # serial rs 485 connection
+        self.client.connect()   # establish connection
+
+        # retreive basic informations from the connected client (Inverter)
+        log.debug('Running Device Information Request')
+        request = ReadDeviceInformationRequest(unit=UNIT)
+        result = self.client.execute(request)
+        log.debug(result)
+
         print('Delta Inverter Data Reader Server started. Polling data via RS-485!')
         time.sleep(1)
+
+    # Class destructor
+    def __del__(self):
+        self.client.close()     # close client connection
 
     # private methode reading data from the inverter
     def __readdata(self):
@@ -59,6 +83,10 @@ class RS485ReaderClass(threading.Thread, sunspec.delta_data_structure.DeltaDataS
 
     # private method updating the data
     def __update(self):
+
+        # read the coils from the the inverter according to the SUNSPEC protocol
+        result = self.client.read_coils(2, 16, unit=UNIT)
+
         self.data['Phase1_Output_Voltage'] = float(random.randrange(2250, 2450, 1)/10)
         self.data['Phase1_Output_Current'] = float(random.randrange(10, 320, 1)/10)
         self.data['Phase1_Output_Frequency'] = float(round(random.random() * 50, 2))
@@ -96,7 +124,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             with open(filename, 'rb') as htmlfile:
                 html = htmlfile.read()
                 self.wfile.write(html)
-                print('Served index.html to the browser')
+                log.log(level=logging.INFO, msg='Sending index.html to the client browser')
 
 
 #
@@ -148,7 +176,7 @@ class WS(threading.Thread):
         try:
             await websocket.recv()                      # receive incoming message
         except websockets.ConnectionClosed:
-            print('Client closed the connection, can not push data!')
+            log.log(level=logging.INFO, msg='Client closed the connection, can\'t push anymore data!')
         finally:
             self.connected.remove(websocket)            # remove client connection if done
 
@@ -204,7 +232,7 @@ class ModBusServer(threading.Thread):
 # ------------------------------------------------------------------
 #
 # create objects from classes
-inverterdata = RS485ReaderClass(RS485_PORT)
+inverterdata = RS485ReaderClass(RS485_DEVICE)
 webpage = WebServerClass(HTTP_HOST_IP, HTTP_PORT)
 websock = WS(inverterdata)
 modbus = ModBusServer(inverterdata)
